f2490ff5af92e71b694354088ab4a811e64cb558
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2024-09-14 23:33:07 +0700

    .

---
+++ Learning-Management/Components/DetailCourses/DetailCourses.jsx
@@ -0,0 +1,15 @@
+
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+
+
+export default function DetailCourses() {
+  return (
+    <div>
+        <Header />
+        <h1>DetailCourses</h1>
+        <Footer />
+        </div>
+  )
+}
